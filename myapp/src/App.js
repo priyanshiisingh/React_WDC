4a91450c0b07998eae1c49b0295bc940a06bb3b6
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-27 19:39:32 +0530

    npm start, installation and app.js

---
+++ myapp/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return ( < div className = "App" >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.Quaerat possimus, distinctio asperiores placeat eum repudiandae ipsa similique nam voluptate neque molestiae blanditiis dignissimos illum quas expedita ab error accusantium culpa ?
+        <
+        h4 > i 'm a heading</h4> < /
+        div >
+    );
+}
+
+export default App;
