fc8dcddde1aa48ba0b401d3cdb7119354fa58546
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-30 19:37:33 +0530

    react components and life cycle dis

--- myapp/src/components/content.js
+++ myapp/src/components/content.js
@@ -1,17 +1,17 @@
 import ListItem from "./listitem";
 
 const Content = () => {
-  const chores = ["item 1", "item 2", "item 3", "item 4"];
-
-  return (
-    <div className="container">
-      <ul>
-        {chores.map((chore) => (
-          <ListItem chore={chore} chores={chores} />
-        ))}
-      </ul>
-    </div>
-  );
+  const data = "from API";
+  return <div className="container">{data}</div>;
 };
 
 export default Content;
+
+{
+  //   const chores = ["item 1", "item 2", "item 3", "item 4"];
+  /* <ul>
+        {chores.map((chore) => (
+          <ListItem chore={chore} chores={chores} />
+        ))}
+      </ul> */
+}
