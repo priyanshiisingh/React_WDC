3c80ae5000714e50a1cbc8ec2b816a14e8d6e4eb
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-11-10 16:49:51 +0530

    enhancer and description HOC

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,13 @@
 import Navbar from "./layouts/navbar";
 import Footer from "./layouts/footer";
 import Content from "./components/content";
+import Description from "./components/Description";
 
 const App = () => {
+  console.log(Description);
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      <Content />
-      {/* <Footer /> */}
+      <Description heading={"heading"} />
     </div>
   );
 };
